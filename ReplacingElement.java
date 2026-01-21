0cdf16dba9d717f4cb315cd45cc0590b5f2808a0
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2026-01-21 15:59:54 +0200

    add Replacing elemnet and helpful math problems

---
+++ ReplacingElement.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class ReplacingElement {
+    /*
+        Authour: Ahmed Hussein
+        Judge: codeforces
+        Link: https://codeforces.com/problemset/problem/1473/A
+        Date: 21/01/2026
+     */
+
+    public static class pair {
+        int first;
+        int second;
+
+        pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+
+
+            pair min = new pair(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+            int n = sc.nextInt();
+            int d = sc.nextInt();
+
+            int max = 0;
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+
+                if (a[i] < min.first && a[i] < min.second) {
+
+                    if (min.first < min.second) {
+                        min.second = a[i];
+                    } else {
+                        min.first = a[i];
+                    }
+                } else if (a[i] < min.second) {
+                    min.second = a[i];
+                } else if (a[i] < min.first) {
+                    min.first = a[i];
+                }
+
+                if (a[i] > max) {
+                    max = a[i];
+                }
+            }
+
+            if (max <= d || min.first + min.second <= d) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+}
---
+++ helpfulMath.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class helpfulMath {
+
+        /*
+        Authour: Ahmed Hussein
+        Judge: codeforces
+        Link: https://codeforces.com/problemset/problem/339/A
+        Date: 21/01/2026
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        int []arr = new int[4];
+
+        int n =0 ;
+        for(int i = 0 ; i< s.length(); i++){
+            if(s.charAt(i) =='1'){
+                arr[1] +=1;
+                n++;
+            }
+            else if(s.charAt(i)=='2'){
+                arr[2] +=1;
+                n++;
+            }
+            else if(s.charAt(i) =='3'){
+                arr[3] +=1;
+                n++;
+            }
+        }
+
+        while(true){
+            if(arr[1]==0 && arr[2]==0 && arr[3]==0){
+                break ;
+            }
+
+            if(arr[1]!=0){
+                System.out.print("1");
+                arr[1]--;
+                if(arr[1]!=0 || arr[2]!=0 || arr[3]!=0){
+                    System.out.print("+");
+
+                }
+                continue;
+            }
+
+            if(arr[2]!=0){
+                System.out.print("2");
+
+                arr[2]--;
+                if(arr[1]!=0 || arr[2]!=0 || arr[3]!=0){
+                    System.out.print("+");
+
+                }
+                continue;
+            }
+            if(arr[3]!=0){
+                System.out.print("3");
+                arr[3]--;
+                if(arr[1]!=0 || arr[2]!=0 || arr[3]!=0){
+                    System.out.print("+");
+
+                }
+            }
+        }
+    }
+}
